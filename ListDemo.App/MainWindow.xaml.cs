d0351fe3336932dab2bf453cbdd0dcc5d076c326
Starinin Andrey <starininandrey@gmail.com>
2022-07-06 18:52:55 +0300

    Init

---
+++ ListDemo.App/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ListDemo.App
+{
+    public partial class MainWindow : Window
+    {
+        private readonly List<string> _list;
+        
+        public MainWindow()
+        {
+            _list = new List<string> {"first", "second", "last"};
+            
+            InitializeComponent();
+
+            List.ItemsSource = _list;
+        }
+
+        private void List_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            InputItem.Text = (string)((ListBox)sender).SelectedItem;
+        }
+
+        private void ButtonAdd_OnClick(object sender, RoutedEventArgs e)
+        {
+            _list.Add(InputItem.Text);
+            
+            List.ItemsSource = null;
+            List.ItemsSource = _list;
+        }
+    }
+}
