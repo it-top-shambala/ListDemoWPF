8a5485c96ee6de4769c531c61e316919406214e1
Starinin Andrey <starininandrey@gmail.com>
2022-07-06 19:08:09 +0300

    Update

--- ListDemo.App/MainWindow.xaml.cs
+++ ListDemo.App/MainWindow.xaml.cs
@@ -24,10 +24,40 @@ namespace ListDemo.App
 
         private void ButtonAdd_OnClick(object sender, RoutedEventArgs e)
         {
+            var input = InputItem.Text;
+
+            if (string.IsNullOrWhiteSpace(input)) return;
+            
             _list.Add(InputItem.Text);
             
             List.ItemsSource = null;
             List.ItemsSource = _list;
+        }
+
+        private void ButtonDelete_OnClick(object sender, RoutedEventArgs e)
+        {
+            var input = InputItem.Text;
+
+            if (string.IsNullOrWhiteSpace(input)) return;
+
+            if(!_list.Remove(input)) return;
+            
+            List.ItemsSource = null;
+            List.ItemsSource = _list;
+        }
+
+        private void InputItem_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(InputItem.Text))
+            {
+                ButtonAdd.IsEnabled = false;
+                ButtonDelete.IsEnabled = false;
+            }
+            else
+            {
+                ButtonAdd.IsEnabled = true;
+                ButtonDelete.IsEnabled = true;
+            }
         }
     }
 }
